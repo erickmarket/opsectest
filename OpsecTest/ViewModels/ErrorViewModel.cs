5d022db16c4580ec41156621559b18525557be9f
Erick M <102126432+erickmarket@users.noreply.github.com>
2023-05-17 11:55:30 -0400

    Initial Commit

---
+++ OpsecTest/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpsecTest.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
